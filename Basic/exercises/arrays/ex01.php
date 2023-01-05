3de2a5760d74095347196231fdb02cc8be66a3d5
anhsaodem <tranxuanhai2107@gmail.com>
2023-01-05 07:23:06 +0700

    arrays functions

---
+++ Basic/exercises/arrays/ex01.php
@@ -0,0 +1,76 @@
+<?php
+// 1. $color = array('white', 'green', 'red', 'blue', 'black');
+// Write a script which will display the following string - Go to the editor
+// "The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
+// and the words 'red', 'green' and 'white' will come from $color.
+
+$color = array('white', 'green', 'red', 'blue', 'black');
+$str= '"The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon"'.'and the words \''.$color[1].'\', \''.$color[2].'\' and \''.$color[0].'\' will come from $color';
+echo $str;
+
+// 2. $color = array('white', 'green', 'red'') Go to the editor
+// Write a PHP script which will display the colors in the following way :
+// Output :
+// white, green, red,
+
+// .green
+// .red
+// .white
+$colors = ['white','green','red'];
+?>
+<ul>
+    <?php
+    foreach($colors as $item){
+        echo '<li>'.$item.'</li>';
+    }
+    ?>
+</ul>
+
+<!-- 3. $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+
+Create a PHP script which displays the capital and country name from the above array $ceu. Sort the list by the capital of the country. Go to the editor
+
+Sample Output :
+The capital of Netherlands is Amsterdam 
+The capital of Greece is Athens 
+The capital of Germany is Berlin 
+- - - - - - - - - - - - - - - - - - - - - - - - -
+- - - - - - - - - - - - - - - - - - - - - - - - - -->
+<?php
+echo '<h3>Ex3</h3>';
+$ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw");
+foreach($ceu as $country=>$capital){
+    echo "The capital of $country is $capital"."</br>";
+}
+?>
+<?php
+echo '<h3>Ex4</h3>';
+// 4. $x = array(1, 2, 3, 4, 5);
+// Delete an element from the above PHP array. After deleting the element, integer keys must be normalized. Go to the editor
+// Sample Output :
+// array(5) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(4) [4]=> int(5) } 
+// array(4) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(5) }
+$x= array(1,2,3,4,5);
+// array_pop($x);
+var_dump($x);
+unset($x[3]);
+var_dump(array_values($x));
+
+// 5. $color = array(4 => 'white', 6 => 'green', 11=> 'red');
+// Write a PHP script to get the first element of the above array. Go to the editor
+// Expected result : white
+$color = array(4 => 'white', 6 => 'green', 11=> 'red');
+echo '<h3>Ex5</h3>';
+echo $color['4'];
+
+// 6. Write a PHP script which decodes the following JSON string. Go to the editor
+// Sample JSON code :
+// {"Title": "The Cuckoos Calling",
+// "Author": "Robert Galbraith",
+// "Detail": {
+// "Publisher": "Little Brown"
+// }}
+// Expected Output :
+// Title : The Cuckoos Calling
+// Author : Robert Galbraith
+// Publisher : Little Brown
--- Basic/exercises/string/ex01.php
+++ Basic/exercises/string/ex01.php
@@ -63,3 +63,58 @@ echo '</br>';
 // Expected Output : 'com'
 
 echo substr($email,-3) ;
+echo '<br>';
+
+// 8. Write a PHP script to format values in currency style. Go to the editor
+// Sample values : value1 = 65.45, value2 = 104.35
+// Expected Result : 169.80
+
+$value1 = 65.45;
+$value2 = 104.35;
+echo number_format($value1+$value2,2,'.');
+echo '</br>';
+
+// 9. Write a PHP script to generate simple random password [do not use rand() function] from a given string. Go to the editor
+// Sample string : '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz'
+// Note : Password length may be 6, 7, 8 etc.
+function password_generate($num_words){
+    $sample_str = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+    $password = substr(str_shuffle($sample_str),0,$num_words);
+return $password;
+}
+echo password_generate(5);
+echo '</br>';
+
+// 10. Write a PHP script to replace the first 'the' of the following string with 'That'. Go to the editor
+// Sample date : 'the quick brown fox jumps over the lazy dog.'
+// Expected Result : That quick brown fox jumps over the lazy dog.
+
+$sample_str = 'the quick brown fox jumps over the lazy dog.';
+echo str_replace('the','That',$sample_str);
+
+// 11. Write a PHP script to find the first character that is different between two strings. Go to the editor
+// String1 : 'football'
+// String2 : 'footboll'
+// Expected Result : First difference between two strings at position 5: "a" vs "o"
+$str1 = 'football';
+$str2 = 'footboll';
+$str_pos = strspn($str1,$str2);
+echo '</br>';
+// echo 'First difference two strings at position '.$str_pos.':"'.$str1[$str_pos].'" vs "'.$str2[$str_pos].'"';
+printf('First difference between two strings at position %d: "%s" vs "%s"',
+$str_pos, $str1[$str_pos], $str2[$str_pos]);
+printf("\n");
+echo '</br>';
+
+// 12. Write a PHP script to put a string in an array. Go to the editor
+// Sample strings : "Twinkle, twinkle, little star,\nHow I wonder what you are.\nUp above the world so high,\nLike a diamond in the sky.";
+// Expected Result (using var_dump()) : array(4) { [0]=> string(30) "Twinkle, twinkle, little star," [1]=> string(26) "How I wonder what you are." [2]=> string(27) "Up above the world so high," [3]=> string(26) "Like a diamond in the sky." }
+$str_samp ="Twinkle, twinkle, little star,\nHow I wonder what you are.\nUp above the world so high,\nLike a diamond in the sky.";
+var_dump(explode(',\n',$str_samp));
+echo '</br>';
+// 13. Write a PHP script to get the filename component of the following path. Go to the editor
+// Sample path : "https://www.w3resource.com/index.php"
+// Expected Output : 'index'
+$path = "https://www.w3resource.com/index.php";
+echo basename($path,'.php');
+echo '</br>';
